507ada46be0c319d70e3dcb97429edc4c2e1426d
Felix Rabe <felix@rabe.io>
2019-07-10 08:46:29 +0200

    wip

--- src/platform_impl/macos/window.rs
+++ src/platform_impl/macos/window.rs
@@ -633,8 +633,7 @@ impl UnownedWindow {
     }
 
     #[inline]
-    /// TODO: Right now set_fullscreen do not work on switching monitors
-    /// in fullscreen mode
+    /// TODO: Does not yet support switching monitors.
     pub fn set_fullscreen(&self, monitor: Option<RootMonitorHandle>) {
         let shared_state_lock = self.shared_state.lock().unwrap();
         if shared_state_lock.is_simple_fullscreen {
